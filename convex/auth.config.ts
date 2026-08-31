e60ed62cbc8faf36182343a7f55f772062e2b1a5
Pranavesh N <pranaveshnandakumar@gmail.com>
2026-08-31 21:33:05 +0530

    Add explicit issuer domains to convex/auth.config.ts

--- convex/auth.config.ts
+++ convex/auth.config.ts
@@ -4,5 +4,13 @@ export default {
       domain: process.env.CONVEX_SITE_URL,
       applicationID: "convex",
     },
+    {
+      domain: "https://quaint-camel-693.convex.site",
+      applicationID: "convex",
+    },
+    {
+      domain: "https://enduring-skunk-350.convex.site",
+      applicationID: "convex",
+    },
   ],
 };
